e664fc66f888411194b6f173a280cf725564e7fa
Gabriel Mendonça <gabrielmendoncanf@gmail.com>
2022-08-09 12:53:50 -0300

    Adicionar função para tratamento de telefones

---
+++ src/phone-number.ts
@@ -0,0 +1,40 @@
+export class PhoneNumber {
+
+  public readonly countryCode: string = '';
+  public readonly ddd: string = '';
+  public readonly number: string = '';
+  public readonly ramal: string = '';
+  public readonly formatedNumber: string;
+  public readonly complete: string;
+
+  constructor(number: string) {
+    this.formatedNumber = number;
+    const parts = number.match(/(\d+)/gm);
+
+    if (!parts) {
+      throw new Error("PhoneNumber is not valid");
+    }
+
+    this.complete = parts.join('');
+    if (this.complete.length === 11) {
+      // ddd + numero
+      this.ddd = parts[0];
+      this.number = parts.filter((_, key) => key !== 0).join('');
+    } else if (this.complete.length > 11) {
+      this.countryCode = parts[0];
+      this.ddd = parts[1];
+      this.number = parts.filter((_, key) => key > 1).join('');
+    }
+  }
+
+  test () {
+    if (this.ddd && this.number) {
+      return true;
+    }
+    return false;
+  }
+}
+
+export const phoneNumber = (number: string) => {
+  return new PhoneNumber(number);
+}
---
+++ test/phone-number.test.ts
@@ -0,0 +1,42 @@
+import { readFile } from "fs/promises";
+import { join } from "path";
+import { phoneNumber } from "../src/phone-number";
+
+const global_content_lines: [string, string][] = [];
+
+describe ("test if is valid number", () => {
+  test.each([
+    ["+55 22 988327317", true],
+    ["+55(22)9.88327317", true],
+    ["5522988327317", true],
+    ["55.22988327317", true],
+    ["+55(22)9.8832-7317", true],
+  ])("phoneNumber(%s).test() => %s", (number, bool) => {
+    expect(phoneNumber(number).test()).toBe(bool);
+  });
+});
+
+describe ("test if is correct ddd, contry code and number", () => {
+
+  const objectResult = {
+    countryCode: '55',
+    ddd: '22',
+    number: '988327317'
+  };
+
+  test.each([
+    ["+55 22 988327317", objectResult],
+    ["+55(22)9.88327317", objectResult],
+    ["5522988327317", objectResult],
+    ["55.22988327317", objectResult],
+    ["+55(22)9.8832-7317", objectResult],
+    ["(22)98832-7317", {...objectResult, countryCode: ''}]
+  ])("phoneNumber(%s).test() => %s", (number, objectResult) => {
+    const result = phoneNumber(number);
+    expect({
+      number: result.number,
+      countryCode: result.countryCode,
+      ddd: result.ddd
+    }).toMatchObject(objectResult);
+  });
+});
